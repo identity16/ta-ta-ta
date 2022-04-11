d0cec53cbfd6ff39443cc4f1ba64a63f2f389137
identity16 <dnjswns0930@gmail.com>
2022-04-11 09:47:43 +0900

    refactor: apply styled-components(Main, App)

--- src/pages/Complete.js
+++ src/pages/Complete.js
@@ -4,38 +4,41 @@ import { Link, useLocation } from "react-router-dom";
 import "./Complete.scss";
 
 function useQuery() {
-  return new URLSearchParams(useLocation().search);
+    return new URLSearchParams(useLocation().search);
 }
 
-export default function Complete() {
-  let query = useQuery();
-  const number = query.get("number");
-  const unit = query.get("unit");
+export default function Complete({ className }) {
+    let query = useQuery();
+    const number = query.get("number");
+    const unit = query.get("unit");
 
-  return (
-    <section className="Complete">
-      <h1 className="Complete__title">타타타</h1>
+    return (
+        <section className={`Complete ${className}`}>
+            <h1 className="Complete__title">타타타</h1>
 
-      <section className="Complete__content">
-        <img
-          src="/img/complete-img.png"
-          alt="Man is checking on checklist beside timer"
-        />
-      </section>
+            <section className="Complete__content">
+                <img
+                    src="/img/complete-img.png"
+                    alt="Man is checking on checklist beside timer"
+                />
+            </section>
 
-      <section className="Complete__button-container">
-        {number !== null && unit !== null ? (
-          <Link
-            to={`/timer/${unit}/${number}`}
-            className="Complete__button Complete__button-restart"
-          >
-            한 번 더 시작
-          </Link>
-        ) : null}
-        <Link to={`/`} className="Complete__button Complete__button-reset">
-          시간 재설정
-        </Link>
-      </section>
-    </section>
-  );
+            <section className="Complete__button-container">
+                {number !== null && unit !== null ? (
+                    <Link
+                        to={`/timer/${unit}/${number}`}
+                        className="Complete__button Complete__button-restart"
+                    >
+                        한 번 더 시작
+                    </Link>
+                ) : null}
+                <Link
+                    to={`/`}
+                    className="Complete__button Complete__button-reset"
+                >
+                    시간 재설정
+                </Link>
+            </section>
+        </section>
+    );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,92 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MainInput from "../components/MainInput";
+import styled from "styled-components";
 
-import "./Main.scss";
+const MainBlock = styled.section`
+    position: relative;
+    margin: 0 auto;
+`;
 
-export default function Main() {
+const Title = styled.h1`
+    margin: 0;
+    padding: 20px 0;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 800;
+    color: #1c1c1c;
+    line-height: 1;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        font-size: 20px;
+        padding: 16px 0;
+    }
+`;
+
+const ContentSection = styled.section`
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+
+    padding: 0 40px;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        top: 47%;
+    }
+`;
+
+const ContentImage = styled.img`
+    display: block;
+    margin: 0 auto 36px;
+    max-width: 420px;
+    width: 100%;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        max-width: 200px;
+        margin-bottom: 18px;
+    }
+`;
+
+const ButtonSection = styled.section`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+
+    padding: 0 24px 56px;
+    box-sizing: border-box;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        padding: 0;
+    }
+`;
+
+const StartButton = styled(Link)`
+    display: block;
+    width: 100%;
+    background-color: #e21e38;
+    color: #fff;
+    font-size: 20px;
+    padding: 20px 0;
+    font-weight: 600;
+    text-decoration: none;
+    line-height: 1;
+    border-radius: 4px;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        font-size: 16px;
+        padding: 16px 0;
+        border-radius: 0;
+    }
+
+    &:hover {
+        background-color: #b91c31;
+    }
+`;
+
+export default function Main({ className }) {
     const defaultNumber = 25;
     const defaultMaxNumber = 60;
     const defaultUnit = "minute";
@@ -52,27 +134,25 @@ export default function Main() {
     }, [number, unit]);
 
     return (
-        <section className="Main">
-            <h1 className="Main__title">타타타</h1>
-
-            <section className="Main__content">
-                <img src="/img/main-img.png" alt="Man grabbing timer pin" />
+        <MainBlock className={className}>
+            <Title>타타타</Title>
+            <ContentSection>
+                <ContentImage
+                    src="/img/main-img.png"
+                    alt="Man grabbing timer pin"
+                />
                 <MainInput
                     number={number}
                     unit={unit}
                     handleNumberChange={handleNumberChange}
                     handleUnitChange={handleUnitChange}
                 />
-            </section>
-
-            <section className="Main__button-container">
-                <Link
-                    to={`/timer/${unit}/${number}`}
-                    className="Main__button-start"
-                >
+            </ContentSection>
+            <ButtonSection>
+                <StartButton to={`/timer/${unit}/${number}`}>
                     시작하기
-                </Link>
-            </section>
-        </section>
+                </StartButton>
+            </ButtonSection>
+        </MainBlock>
     );
 }
