4f08e00086e473c7f53201079059d519752566dd
identity16 <dnjswns0930@gmail.com>
2022-04-12 09:45:24 +0900

    refactor: apply styled-components(Complete)

--- src/pages/Complete.js
+++ src/pages/Complete.js
@@ -1,7 +1,94 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
 
-import "./Complete.scss";
+const CompleteBlock = styled.section`
+    margin: 0 auto;
+    position: relative;
+`;
+
+const Title = styled.h1`
+    margin: 0;
+    padding: 20px 0;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 800;
+    color: #1c1c1c;
+    line-height: 1;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        font-size: 20px;
+        padding: 16px 0;
+    }
+`;
+
+const ContentSection = styled.section`
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+
+    padding: 0 40px;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        top: 50%;
+    }
+`;
+
+const ContentImage = styled.img`
+    display: block;
+    margin: 0 auto 36px;
+    max-width: 420px;
+    width: 100%;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        max-width: 220px;
+        margin-bottom: 18px;
+    }
+`;
+
+const ButtonSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    gap: 8px;
+
+    padding: 0 24px 56px;
+    box-sizing: border-box;
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        padding: 0;
+        flex-direction: row;
+        gap: 0;
+    }
+`;
+
+const Button = styled(Link)`
+    width: 100%;
+    font-size: 20px;
+    font-weight: 600;
+    padding: 20px 0;
+    color: #fff;
+    border-radius: 4px;
+    line-height: 1;
+    text-decoration: none;
+    background-color: ${(props) => props.background};
+    color: ${(props) => props.text};
+
+    @media ${(props) => props.theme.hMedia.sm} {
+        font-size: 16px;
+        padding: 16px 0;
+        border-radius: 0;
+    }
+
+    &:hover {
+        background-color: ${(props) => props.hover};
+    }
+`;
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -13,32 +100,34 @@ export default function Complete({ className }) {
     const unit = query.get("unit");
 
     return (
-        <section className={`Complete ${className}`}>
-            <h1 className="Complete__title">타타타</h1>
-
-            <section className="Complete__content">
-                <img
+        <CompleteBlock className={`Complete ${className}`}>
+            <Title>타타타</Title>
+            <ContentSection>
+                <ContentImage
                     src="/img/complete-img.png"
                     alt="Man is checking on checklist beside timer"
                 />
-            </section>
-
-            <section className="Complete__button-container">
-                {number !== null && unit !== null ? (
-                    <Link
+            </ContentSection>
+            <ButtonSection>
+                {number && unit && (
+                    <Button
                         to={`/timer/${unit}/${number}`}
-                        className="Complete__button Complete__button-restart"
+                        background="#e21e38"
+                        hover="#b91c31"
+                        text="#fff"
                     >
                         한 번 더 시작
-                    </Link>
-                ) : null}
-                <Link
+                    </Button>
+                )}
+                <Button
                     to={`/`}
-                    className="Complete__button Complete__button-reset"
+                    background="#49576a"
+                    hover="#1f252a"
+                    text="#fff"
                 >
                     시간 재설정
-                </Link>
-            </section>
-        </section>
+                </Button>
+            </ButtonSection>
+        </CompleteBlock>
     );
 }
