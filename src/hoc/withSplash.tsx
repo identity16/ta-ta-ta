895cedd1a1a3ffa8857af8428ff775fda141ed4f
Wonjun Shin(Joon) <dnjswns0930@gmail.com>
2022-12-18 13:07:24 +0900

    fix: withSplash.jsx => withSplash.tsx

--- src/hoc/withSplash.tsx
+++ src/hoc/withSplash.tsx
@@ -1,11 +1,16 @@
-import { useState, useEffect, useContext } from 'react';
+import {
+  useState,
+  useEffect,
+  useContext,
+  ComponentProps,
+  ComponentType,
+} from 'react';
 import Splash from '../components/Splash';
 import { SplashContext } from '../contexts/splash';
 import useViewportSize from '../hooks/useViewportSize';
 
-const withSplash =
-  (WrappedComponent, duration = 2500) =>
-  props => {
+function withSplash(WrappedComponent: ComponentType, duration: number = 2500) {
+  return (props: ComponentProps<any>) => {
     const { width, height } = useViewportSize();
     const [loading, setLoading] = useState(true);
     const { shown, setShownFlag } = useContext(SplashContext);
@@ -35,5 +40,6 @@ const withSplash =
       <WrappedComponent {...props} />
     );
   };
+}
 
 export default withSplash;
