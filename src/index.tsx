67d86bd09aac16fe487e2a166874c4cc0b116711
Wonjun Shin(Joon) <dnjswns0930@gmail.com>
2022-12-20 23:18:45 +0900

    env: upgrade react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import theme from './style/theme';
 import GlobalStyle from './style/GlobalStyle';
 import { SplashProvider } from './contexts/splash';
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
+  <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <SplashProvider>
         <App />
       </SplashProvider>
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
